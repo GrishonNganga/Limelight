60d1db9f0869b6cd6ca6a6bdf0ec304293a238d8
BTG001 <brunokipkorir2@gmail.com>
2020-05-20 14:43:57 +0300

    Using Javascript to create Community constructor which constitutes of the community name, users, posts, links and comments as paramters

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function Community(name, users, post, links, comments) {
+  this.communityName = name;
+  this.users = users;
+  this.post = post;
+  this.links = links;
+  this. comments = comments;
+}
