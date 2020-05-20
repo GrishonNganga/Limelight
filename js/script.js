e94639cf52cc611de6b25cf1eb815612eb5c6c3e
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-05-20 21:56:11 +0300

    Revert "Add communities section."

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-// START of jquery code for trendingProjects CAROUSEL
-/ Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-  interval: false
-});
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-$('.multi-item-carousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-
-  if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo($(this));
-  } else {
-  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
-});
-// END of jquery code for trendingProjects CAROUSEL
 
 function Community(name, user, post, links, comments) {
   this.communityName = name;
