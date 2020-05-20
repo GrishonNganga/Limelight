67a9feadb89264fba2f42bc44c5a2ec4f886f818
BTG001 <brunokipkorir2@gmail.com>
2020-05-20 15:10:36 +0300

    To access the name of the user

--- js/script.js
+++ js/script.js
@@ -8,3 +8,5 @@ function Community(name, user, post, links, comments) {
 var newCommunity = new Community("techCommunity", "Bruno", "Check out my new project", "https://btg001.github.io/pizzeria/", "This is really nice!");
 console.log(newCommunity);
 console.log(newCommunity.communityName);
+console.log(newCommunity.user);
+
