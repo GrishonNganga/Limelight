4fda78fab3f0137146c13c55f97abdb0f0a8c68f
BTG001 <brunokipkorir2@gmail.com>
2020-05-20 15:04:43 +0300

    Testing my community constructor if it works

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-function Community(name, users, post, links, comments) {
+function Community(name, user, post, links, comments) {
   this.communityName = name;
-  this.users = users;
+  this.user = user;
   this.post = post;
   this.links = links;
   this. comments = comments;
 }
+var newCommunity = new Community("techCommunity", "Bruno", "Check out my new project", "https://btg001.github.io/pizzeria/", "This is really nice!");
+console.log(newCommunity);
