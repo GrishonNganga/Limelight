4160665a67102491cdce66fa30c7e76ac6f635ce
BTG001 <brunokipkorir2@gmail.com>
2020-05-21 19:55:17 +0300

    initialize firebase

--- js/script.js
+++ js/script.js
@@ -42,3 +42,33 @@ $(document).ready(function() {
     event.preventDefault();
   })
 })
+
+
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyBqKX1_Jmhm809kqW3SBifAjmVfzLfC-0w",
+  authDomain: "comments2-a5f2e.firebaseapp.com",
+  databaseURL: "https://comments2-a5f2e.firebaseio.com",
+  projectId: "comments2-a5f2e",
+  storageBucket: "comments2-a5f2e.appspot.com",
+  messagingSenderId: "617710094361"
+};
+firebase.initializeApp(config);
+//Rootref is the whole database.
+const rootRef = firebase.database().ref();
+//commentsRef is just the pageCountsNode
+const commentsRef = rootRef.child('comments');
+//Listen for click on Submit Comment button, and post comment.
+document.getElementById("btnSubmitComment").addEventListener("click", function () {
+  //Replace line breaks in comment with br tags.
+  var newcomment = document.getElementById('txComment').value.replace(/\n/g, "<br>");
+  //Define a new, keyed post.
+  var newPostRef = commentsRef.push();
+  //Fill tne new keyed post with data
+  newPostRef.set({
+      name: document.getElementById('tbName').value.trim(),
+      comment: newcomment.trim(),
+      frompage: location.pathname,
+      when: firebase.database.ServerValue.TIMESTAMP
+  });
+});
