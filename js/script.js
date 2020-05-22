425cde87af8180932b9771776539134259a01a84
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-05-22 07:02:07 +0300

    Add Content and polish up with everyone's work.

--- js/script.js
+++ js/script.js
@@ -74,6 +74,7 @@ var strathGeeks = new Community("Strath Geeks", "C:\Moringa\Limelight\images\cut
 var moiUniDevs = new Community("Moi Uni Devs", "C:\Moringa\Limelight\images\cute-lime.jpg");
 var usiuTitans = new Community("USIU Titans", "C:\Moringa\Limelight\images\cute-lime.jpg");
 
+var communities = [communityAll, jkuatHackers, uonDevs, tukTechTurks,dekutMasters, strathGeeks, moiUniDevs, usiuTitans];
 
 
 var grishon = new User("Grishon", "Ng'ang'a","grishonnganga", "grishon.nganga01@gmail.com", "123@Iiht", communityAll);
@@ -82,19 +83,22 @@ var arnold = new User("Arnold", "Lifereze", "arnold@gmail.com", "123@Iiht", comm
 var josphat = new User("Josphat", "Mwangi", "josphat.kungu@gmail.com", "123@Iiht", communityAll);
 var bruno = new User("Bruno", "Kiplang'at", "bruno.kiplangat@gmail.com", "123@Iiht", communityAll);
 var maryann = new User("Maryann", "Makena", "maryanne.makena@gmail.com", "123@Iiht", communityAll);
+var sospeter = new User("Sospeter", "Kung'u", "sospeter.kungu@gmail.com", "123@Iiht", communityAll);
 
 var users = [grishon, karen, arnold, josphat, bruno, maryann];
 
 console.log(users);
 
-var grishonPost = new Post(grishon, "Check out my account! I am cool", communityAll, "https://github.com/grishonnganga/");
-var karenPost = new Post(karen, "https://github.com/grishonnganga",communityAll);
-var arnoldPost = new Post(arnold, "https://github.com/grishonnganga",communityAll);
-var josphatPost = new Post(josphat, "https://github.com/grishonnganga",communityAll);
-var brunoPost = new Post(bruno, "https://github.com/grishonnganga",communityAll);
-var maryannPost = new Post(maryann, "https://github.com/grishonnganga",communityAll);
+var limelightPost = new Post(grishon, "We built this for us. So ofcourse the first post will be ours!!", moiUniDevs, "https://grishonnganga.github.io/Portfolio/");
+var grishonPost = new Post(grishon, "In an effort to put myself out there I made my portfolio site. Please tell me what you think of it.", communityAll, "https://grishonnganga.github.io/Portfolio/");
+var karenPost = new Post(karen, "I think it's very important to get more women into computing. My slogan is: Computing is too important to be left to men. Check my work.",strathGeeks, "https://karenngala.github.io/Portfolio/");
+var arnoldPost = new Post(arnold, "My tech journey is none to be played with. So watch this space. Look at this work in 15 weeks.",communityAll, "https://lifereze.github.io/portfolio-IP/");
+var josphatPost = new Post(josphat, "Better known as Jopa. Young ninja not to mess with. Check my work and hmu for collabs!",communityAll,"https://josphat-mwangi.github.io/Portfolio/");
+var brunoPost = new Post(bruno, "There is a reason they call me BTG. Ask and might be granted.",communityAll, "https://btg001.github.io/my-portforlio/");
+var maryannPost = new Post(maryann, "I don't fancy much. A good life and super loud bike is all. Oh! I make these projects too. So check out my work!!",communityAll, "https://makena-maryann.github.io/portfolio-landing-page/");
+var sospeterPost = new Post(sospeter, "Open your code. Code never lies!",communityAll, "https://sospeterkungu.github.io/resubmission-myportfoliowk1/");
 
-var posts = [grishonPost,karenPost, arnoldPost, josphatPost, brunoPost, maryannPost];
+var posts = [limelightPost, grishonPost,karenPost, arnoldPost, josphatPost, brunoPost, maryannPost];
 
 
 
@@ -106,7 +110,9 @@ $(document).ready(function() {
   //Initialize a variable. If it is false. User has not logged in. If true...
   var logIn = false;
     //Post Comments...
+    var postsCount = 0;
     posts.forEach((post)=>{
+      postsCount++;
       var user = post.user.f_name;
       var message = post.message;
       var link = post.link;
@@ -119,9 +125,21 @@ $(document).ready(function() {
         counter++;
       });
 
-      $('.posts').html($('.posts').html() + "<div class=\"container card card-post\"><a href="+link+" target=\"_blank\" rel=\"noopener noreferrer\" class=\"text-dark font-weight-bold\"><div class=\"row\"><div class=\"col-2\"><div class=\"upVote\"><i class=\"fa fa-caret-up fa-2x\" ></i></div><div class=\"votesNumber\">200</div><div class=\"downVote\"><i class=\"fa fa-caret-down fa-2x\" ></i></div></div><div class=\"col\"><div class=\"row\"><div class=\"col\"><p class=\"owner\">Posted by" +user+ " 2 hours ago</p></div></div><div class=\"row\"><div class=\"col\">"+message+"</div></div><div class=\"row feedback-section\"><div class=\"col\"><i class=\"fa fa-comment\">"+commentsCounter+ "comments</i></div><div class=\"col\"><li class=\"fa fa-star\">"+commentStars+ "stars</li></div></div></div></div> </a></div>");
+      $('.posts').html($('.posts').html() + "<div class=\"container "+postsCount+ " card card-post\"><a href="+link+" target=\"_blank\" rel=\"noopener noreferrer\" class=\"text-dark font-weight-bold\"><div class=\"row\"><div class=\" col-2 col-sm-1\"><div class=\"upVote\"><i class=\"fa fa-caret-up fa-2x\" ></i></div><div class=\"votesNumber\">200</div><div class=\"downVote\"><i class=\"fa fa-caret-down fa-2x\" ></i></div></div><div class=\"col\"><div class=\"row\"><div class=\"col\"><p class=\"owner\">Posted by " +user+ " 2 hours ago</p></div></div><div class=\"row\"><div class=\"col\">"+message+"</div></div><div class=\"row feedback-section\"><div class=\"col\"><i class=\"fa fa-comment\">"+commentsCounter+ "comments</i></div><div class=\"col\"><li class=\"fa fa-star\">"+commentStars+ "stars</li></div></div></div></div> </a></div>");
 
     });
+
+    var communitiesCount = 0;
+    communities.forEach((community)=>{
+      communitiesCount++;
+
+      var name = community.communityName;
+      $('.card-text').html($('.card-text').html() + "<p class=\"card-text\"><h6 class=\"font-weight-bold\">"+communitiesCount+"      "+name+"</h6>");
+    });
+
+
+
+
 
     $(".btn").click(function() {
       $(".hidden").show();
@@ -142,6 +160,19 @@ $(document).ready(function() {
         }
       });      
     
+      var trackerr = 0;
+      posts.forEach((post)=>{
+        trackerr++;
+          $('.'+trackerr.toString()).click((event)=>{
+            var id = event.currentTarget;
+            console.log(id);
+            id.attr('class').split(' ').map(function(clssName){     
+              $("#message").append(clssName +" ");
+              console.log(clssName);
+            });
+          // console.log(tracker);
+          });
+        });
 });
 
 $(document).ready(()=>{
@@ -165,6 +196,10 @@ $(document).ready(()=>{
         }
       })
     });
+
+    // users.forEach(()=>{
+
+    // })
 });
 
 
