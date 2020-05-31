34a1520d4117db202ab9aa5d730fd51e225c4d3d
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-05-31 17:49:32 +0300

    Add upload images UI and replace the upload textareas with CKEditor textarea.

--- js/upload.js
+++ js/upload.js
@@ -17,17 +17,24 @@ function checkStatus(){
 $(document).ready(()=>{
     $('.body-div').click(()=>{
         $('.live').hide();
+        $('.images').hide();
         $('.body').show();
         $('.post-img').removeClass('inactive-img');
         $('.post-img').addClass('active-img');
         $('.link-img').removeClass('active-img');
         $('.link-img').addClass('inactive-img');
+        $('.images-img').removeClass('active-img');
+        $('.images-img').addClass('inactive-img');
         $('.body-div').removeClass('inactive');
         $('.body-div').addClass('active');
         $('.live-div').removeClass('active');
         $('.live-div').addClass('inactive');
+        $('.images-div').removeClass('active');
+        $('.images-div').addClass('inactive');
         $('.live-txt').removeClass('active-txt');
         $('.live-txt').addClass('inactive-txt');
+        $('.images-txt').removeClass('active-txt');
+        $('.images-txt').addClass('inactive-txt');
         $('.body-txt').removeClass('inactive-txt');
         $('.body-txt').addClass('active-txt');
         
@@ -35,19 +42,50 @@ $(document).ready(()=>{
 
     $('.live-div').click(()=>{
         $('.body').hide();
+        $('.images').hide();
         $('.live').show();
         $('.link-img').removeClass('inactive-img');
         $('.link-img').addClass('active-img');
         $('.post-img').removeClass('active-img');
         $('.post-img').addClass('inactive-img');
+        $('.images-img').removeClass('active-img');
+        $('.images-img').addClass('inactive-img');
         $('.live-div').removeClass('inactive');
         $('.live-div').addClass('active');
         $('.body-div').removeClass('active');
         $('.body-div').addClass('inactive');
+        $('.images-div').removeClass('active');
+        $('.images-div').addClass('inactive');
         $('.body-txt').removeClass('active-txt');
         $('.body-txt').addClass('inactive-txt');
+        $('.images-txt').removeClass('active-txt');
+        $('.images-txt').addClass('inactive-txt');
         $('.live-txt').removeClass('inactive-txt');
         $('.live-txt').addClass('active-txt');
+    });
+
+    $('.images-div').click(()=>{
+        $('.body').hide();
+        $('.live').hide();
+        $('.images').show();
+        $('.images-img').removeClass('inactive-img');
+        $('.images-img').addClass('active-img');
+        $('.post-img').removeClass('active-img');
+        $('.post-img').addClass('inactive-img');
+        $('.link-img').removeClass('active-img');
+        $('.link-img').addClass('inactive-img');
+        $('.images-div').removeClass('inactive');
+        $('.images-div').addClass('active');
+        $('.body-div').removeClass('active');
+        $('.body-div').addClass('inactive');
+        $('.live-div').removeClass('active');
+        $('.live-div').addClass('inactive');
+        $('.body-txt').removeClass('active-txt');
+        $('.body-txt').addClass('inactive-txt');
+        $('.live-txt').removeClass('active-txt');
+        $('.live-txt').addClass('inactive-txt');
+        $('.images-txt').removeClass('inactive-txt');
+        $('.images-txt').addClass('active-txt');
     });
 
     $('.cancel-btn').click(()=>{
@@ -57,7 +95,7 @@ $(document).ready(()=>{
     $(".upload-btn").click(function () {
         $('.upload-btn').toggle();
         $(".upload-loading-btn").toggle();
-        var body = document.getElementById('body').value;
+        var body = CKEDITOR.instances['body'].getData();
         var live = document.getElementById('live').value;
         if(body === ""){
             $('.upload-btn').toggle();
