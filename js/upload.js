d66134a1e6b8d45b774f04541ea3c29c4b58b4ab
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-06-05 02:27:18 +0300

    Fix the whole images upload in upload.html and include username showing in upload.html

--- js/upload.js
+++ js/upload.js
@@ -1,20 +1,15 @@
 $(document).ready(() => {
-    let uid;
-    let username;
-    let imageDisplay;
-    let images = [];
-    let imageNames = [];
-    let database = firebase.database();
-    let storage = firebase.storage();
     let user;
-    let postsImages = storage.ref("posts");
-    let newPost = database.ref('posts');
     let body;
     let live;
-    let counter = 0;
+    let images;
+
+    //Check if user is logged in or not. If user is not logged in.
     checkStatus();
+
+    //Set listener for images uploaded.
     document.getElementById('file').onchange = (event) => {
-        loadFile(event)
+        images = loadFile(event);
     }
 
     $('.body-div').click(() => {
@@ -127,7 +122,7 @@ $(document).ready(() => {
             document.getElementById("errors").style.color = "red";
             return;
         }
-        if (imageDisplay == null) {
+        if (images == null) {
             $('.upload-btn').toggle();
             $(".upload-loading-btn").toggle();
             textError = "Upload at least one image!";
@@ -144,119 +139,124 @@ $(document).ready(() => {
             return;
         }
 
-        
-        
         if (user == null) {
             $('.upload-btn').toggle();
             $(".upload-loading-btn").toggle();
+            window.location.href = "login.html";
             console.log("Nothing to be done here there is no user!");
         } else {
             uid = user.uid;
-            
 
-            //Saves the images to firebase storage...
-            Promise.all(
-                images.map((image)=>{
-                    uploadFileToStorage(image, saveToDB);
-                })
-            )
-            .catch((err)=>{
-                console.log("Some failed. Error is "+err.message);
-            });
+            //Upload the images and get their file paths!
+            let imagesArr = uploadImagesToFirebaseStorage(images);
+            
+            uploadImagesToFirebaseDatabase(imagesArr, uid);
 
         }
 
     });
 
     //This checks if user is logged in or not. If logged in. Sets the global variables.
-    function checkStatus() {
-        firebase.auth().onAuthStateChanged((userLoggedIn) => {
-            if (userLoggedIn) {
-                user = userLoggedIn;
-                console.log("User logged in!");
-            } else {
-                console.log("No user logged in.");
-                window.location.href = "login.html";
-            }
-        })
     
-    }
-
-
-    function uploadFileToStorage(file, saveToDB){
-        var name = new Date() + "-"+file.name;
-        var metadata = {
-            contentType: file.type
-        }
-        postsImages.child(name).put(file, metadata)
-        .then((snapshot)=>{
-            const data = snapshot.metadata.fullPath;
-            console.log(data);
-            saveToDB(data);
-        })
-        .catch((err)=>{
-            console.log("Something went wrong! "+err.message);
-        });
-    }
-
-
-    function saveToDB(image){
-        counter++;
-        database.ref("users/" + uid).once("value", (usernameToGet) => {
-            username = usernameToGet.val().name;
-            console.log("The image is not null" +image);
-
-            //This code should be refactored with legit promises...
-
-            newPost.push({
-                user: uid,
-                username: username,
-                body: body,
-                live: live,
-                timestamp: firebase.database.ServerValue.TIMESTAMP,
-                images: imageNames
-            }).then((snap)=>{
-                console.log("This is the post thst has been added " +snap);
-                console.log("Added post key is "+snap.key);
-                database.ref("userPost/" + uid).child(snap.key).set(snap.key).then(() => {
-                    window.location.href = "index.html";
-                    $('.upload-btn').toggle();
-                    $(".upload-loading-btn").toggle();
-                });
-            }).catch((err)=>{
-                console.log("Post was not successful due to "+err.message);
-            });
-        });
-    }
-
-
-    function loadFile(event) {
-        $('.image-preview').show();
-        var image = document.getElementById('imageUpload');
-        let imageList = document.getElementById('addPic');
-
-
-        imageDisplay = event.target.files;
-        if (imageDisplay.length > 0) {
-            console.log(imageDisplay);
-            for (let i = 0; i < imageDisplay.length; i++) {
-                let imageLi = document.createElement('li');
-                let imgElement = document.createElement('img');
-                let image = imageDisplay[i];
-                const imageURL = window.URL.createObjectURL(image);
-                console.log(imageURL);
-                imgElement.setAttribute('src', imageURL);
-                imageLi.appendChild(imgElement);
-                imageList.appendChild(imageLi);
-
-                images.push(imageDisplay[i]);
-                imageNames.push("posts/"+imageDisplay[i].name);
-
-            }
-        } else {
-            $('.image-preview').hide();
-        }
-
-
-    };
+    
 });
+
+//Check if user is logged in or not!
+function checkStatus() {
+    let caretUserDiv = document.getElementById("caret-username");
+    caretUserDiv.innerHTML = "Anonymous";
+    firebase.auth().onAuthStateChanged(async (userLoggedIn) => {
+        if (userLoggedIn) {
+
+            await firebase.database().ref("users/" + userLoggedIn.uid).once("value", (usernameToGet) => {
+                username = usernameToGet.val().name;
+
+            });
+            user = userLoggedIn;
+            caretUserDiv.innerHTML = username;
+        } else {
+            console.log("No user logged in.");
+            window.location.href = "login.html";
+        }
+    })
+
+}
+
+
+//Load the files selected by a user. To populate accordingly on the UI!
+function loadFile(event) {
+    $('.image-preview').show();
+    let imageList = document.getElementById('addPic');
+
+    imageDisplay = event.target.files;
+    if (imageDisplay.length > 0) {
+        console.log(imageDisplay);
+        for (let i = 0; i < imageDisplay.length; i++) {
+            let imageLi = document.createElement('li');
+            let imgElement = document.createElement('img');
+            let image = imageDisplay[i];
+            const imageURL = window.URL.createObjectURL(image);
+            imgElement.setAttribute('src', imageURL);
+            imageLi.appendChild(imgElement);
+            imageList.appendChild(imageLi);
+
+        }
+        console.log(imageDisplay.toString());
+    } else {
+        $('.image-preview').hide();
+    }
+
+    return imageDisplay;
+
+}
+
+//Upload selected files to database storage.
+function uploadImagesToFirebaseStorage(images){
+    let allImgs = [];
+    try{
+        
+        const imagesURLS = Promise.all(
+            [...images].map((image)=>{
+                let metadata = {
+                    contentType: image.type
+                }
+                let name = new Date() + "-" +image.name;
+                let date = new Date().getMonth().toString()
+                const savedSnapshot = firebase.storage().ref("posts").child(date).child(name).put(image,metadata)
+
+                savedSnapshot.catch((err)=>{console.log(err.message)})
+
+                allImgs.push(savedSnapshot.snapshot.ref.fullPath);
+            })
+        )
+    }catch(err){
+        console.log(err.message);
+    }
+    return allImgs;
+}
+
+
+//Upload the name to the files uploaded to the db.
+function uploadImagesToFirebaseDatabase(image, uid){
+    firebase.database().ref("users/" + uid).once("value", (usernameToGet) => {
+        username = usernameToGet.val().name;
+
+        firebase.database().ref("posts").push({
+            user: uid,
+            username: username,
+            body: body,
+            live: live,
+            timestamp: firebase.database.ServerValue.TIMESTAMP,
+            images: image
+        }).then((snap)=>{
+            firebase.database().ref("userPost/" + uid).child(snap.key).set(snap.key).then(() => {
+                //window.location.href = "index.html";
+                $('.upload-btn').toggle();
+                $(".upload-loading-btn").toggle();
+                // window.location.href = "index.html";
+            });
+        }).catch((err)=>{
+            console.log("Post was not successful due to "+err.message);
+        });
+    });
+}
