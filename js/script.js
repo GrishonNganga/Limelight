6d9d2a4bc0dd57f5698fb10b469e3e44d7ff1171
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-06-05 20:58:44 +0300

    Small addition to search functionality.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var user;
-var posts;
+var postsForFilter;
 
 window.onload = ()=>{
 
@@ -10,7 +10,21 @@ window.onload = ()=>{
 
 $(document).ready(()=>{
 
-  loadPosts();
+  launchUI();
+
+  $('#search-btn').click((event)=>{
+    event.preventDefault();
+    let search = document.getElementById('search-input').value;
+    // const filteredPosts = postsForFilter.map((post)=>{
+    //   if(post.body.includes(search)){
+    //     return post;
+    //   }
+    // });
+    console.log(postsForFilter);
+
+    //console.log(filteredPosts);
+  });
+
 
   //Carousel...
   $(".all-posts").slick({
@@ -47,37 +61,6 @@ $(document).ready(()=>{
     ],
   });
 
-  $("#url-upload").click(function () {
-    var database = firebase.database();
-    var user = firebase.auth().currentUser;
-    if(user == null){
-      console.log("Nothing to be done here there is no user!");
-    }else{
-      var uid = user.uid;
-      var username;
-      database.ref("users/" +uid).once("value", (usernameToGet)=>{
-        username = usernameToGet.val().name;
-
-        //Todo This code should be refactored with legit promises...
-
-        var newPost = database.ref("posts");
-
-        var addedPost = newPost.push({
-          user: uid,
-          username: username,
-          body: "This is the body",
-          live: "This is the livelink.",
-          timestamp: firebase.database.ServerValue.TIMESTAMP
-        });
-
-        var postId = addedPost.key;
-        console.log(postId);
-
-        database.ref("userPost/"+uid).child(postId).set(postId);
-      });
-    }
-    
-  });
   document.getElementById('logout').onclick = (event)=>{
     event.preventDefault();
     firebase.auth().signOut().then(() => {
@@ -105,7 +88,6 @@ function checkStatus(){
       
     } else {
       // User is signed out.
-      console.log("No user logged in!");
       caretUserDiv.innerHTML = "Anonymous";
       //User is not logged in.
     
@@ -113,104 +95,109 @@ function checkStatus(){
     }
   });
 }
-function loadPosts(){
+
+
+function displayPosts(posts){
+  posts.forEach((post)=>{
+  
+
+    var postId = post.key;
+   var post = post.valueOf("body").val();
+   console.log(post);
+   var message = post.body;
+   var link = post.live;
+   var user = post.username;
+   var timestamp = new Date(post.timestamp);
+   var when;
+   var postedAgoText;
+   var commentsCounter = 0;
+  
+   var date = new Date();
+   var year = date.getFullYear();
+   var month = date.getMonth();
+   var dayOfMonth = date.getDay();
+   var hourOfDay = date.getHours();
+   var minsOfHour = date.getMinutes();
+   var secsOfMins = date.getSeconds();
+
+   var postYear = timestamp.getFullYear();
+   var postMonth = timestamp.getMonth();
+   var postDay = timestamp.getDay();
+   var postTimeMins = timestamp.getMinutes();
+   var postTimeHours = timestamp.getHours();
+   var postTimeSecs = timestamp.getSeconds();
+
+   if(year === postYear){
+       if(month === postMonth){
+         if(dayOfMonth === postDay){
+           if(hourOfDay === postTimeHours){
+             if(minsOfHour === postTimeMins){
+               when = secsOfMins - postTimeSecs ;
+               postedAgoText = "seconds ago";
+             }else if(minsOfHour - postTimeMins === 1){
+                 when = minsOfHour - postTimeMins ;
+                 postedAgoText = "minute ago";
+             }else{
+                 when = minsOfHour - postTimeMins ;
+                 postedAgoText = "minutes ago";   
+             }
+           }else if(hourOfDay - postTimeHours === 1){
+               when = hourOfDay - postTimeHours ;
+               postedAgoText = "hour ago";
+           }else{
+               when = hourOfDay - postTimeHours ;
+               postedAgoText = "hours ago";   
+           }
+         }else if(dayOfMonth - postDay === 1){
+             when = dayOfMonth -  postDay;
+             postedAgoText = "day ago";
+         }else{
+           when = dayOfMonth - postDay ;
+           postedAgoText = "days ago";
+         }
+       }else if(month - postMonth === 1){
+           when = month - postMonth ;
+           postedAgoText = "month ago";
+       }
+       else{
+           when = month - postMonth;
+           postedAgoText = "months ago";
+       }
+   }else if(year - postYear === 1){
+       when = year - postYear ;
+       postedAgoText = "year ago";
+   }
+   else{
+       when = year - postYear ;
+       postedAgoText = "years ago";
+   }
+
+   $(".posts").html(
+     $(".posts").html() +
+       '<div class="container card card-post"><div class="row"><div class=" col-2 col-sm-1"><div class="upVote"><i class="fa fa-caret-up fa-2x" ></i></div><div class="votesNumber">200</div><div class="downVote"><i class="fa fa-caret-down fa-2x" ></i></div></div><div class="col"><a href='+link+' target ="_blank" rel="noopener noreferrer"><div class="row"><div class="col"><p class="owner">Posted by ' +
+       user + ' ' +
+       when +" "+postedAgoText+'</p></div></div><div class="row"><div class="col">' + 
+       message +
+       '</div></div><div class="row feedback-section"><a href="post.html?post='+postId+'"><div class="col"><i class="fas fa-comment-alt"> ' +
+       commentsCounter +
+       ' comments</i></div></a><div class="col"><i class="fa fa-flag offset-5">' +
+       " report</i></div></div></a></div></div></div>"
+   );
+ });
+}
+
+async function launchUI(){
+  //Get data from database.
   $('.spinner-grow').toggle();
+  const posts = await getPostsFromDB();
+
+  postsForFilter = posts;
+  //Pass gotten data to be displayed...
+  displayPosts(posts);
+  $('.spinner-grow').toggle();
+}
+
+function getPostsFromDB(){
   var database = firebase.database();
-  // database.
-  database.ref("posts").once('value', function(posts) {
-    console.log(posts.val());
-    var postsCount = 0;
-    var commentsCounter = 0;
-
-    $('.spinner-grow').toggle();
-    posts.forEach((post)=>{
-       postsCount++;
-
-       var postId = post.key;
-     
-      var post = post.valueOf("body").val();
-      console.log(post);
-      var message = post.body;
-      var link = post.live;
-      var user = post.username;
-      var timestamp = new Date(post.timestamp);
-      var when;
-      var postedAgoText;
-     
-      var date = new Date();
-      var year = date.getFullYear();
-      var month = date.getMonth();
-      var dayOfMonth = date.getDay();
-      var hourOfDay = date.getHours();
-      var minsOfHour = date.getMinutes();
-      var secsOfMins = date.getSeconds();
-
-      var postYear = timestamp.getFullYear();
-      var postMonth = timestamp.getMonth();
-      var postDay = timestamp.getDay();
-      var postTimeMins = timestamp.getMinutes();
-      var postTimeHours = timestamp.getHours();
-      var postTimeSecs = timestamp.getSeconds();
-
-      if(year === postYear){
-          if(month === postMonth){
-            if(dayOfMonth === postDay){
-              if(hourOfDay === postTimeHours){
-                if(minsOfHour === postTimeMins){
-                  when = secsOfMins - postTimeSecs ;
-                  postedAgoText = "seconds ago";
-                }else if(minsOfHour - postTimeMins === 1){
-                    when = minsOfHour - postTimeMins ;
-                    postedAgoText = "minute ago";
-                }else{
-                    when = minsOfHour - postTimeMins ;
-                    postedAgoText = "minutes ago";   
-                }
-              }else if(hourOfDay - postTimeHours === 1){
-                  when = hourOfDay - postTimeHours ;
-                  postedAgoText = "hour ago";
-              }else{
-                  when = hourOfDay - postTimeHours ;
-                  postedAgoText = "hours ago";   
-              }
-            }else if(dayOfMonth - postDay === 1){
-                when = dayOfMonth -  postDay;
-                postedAgoText = "day ago";
-            }else{
-              when = dayOfMonth - postDay ;
-              postedAgoText = "days ago";
-            }
-          }else if(month - postMonth === 1){
-              when = month - postMonth ;
-              postedAgoText = "month ago";
-          }
-          else{
-              when = month - postMonth;
-              postedAgoText = "months ago";
-          }
-      }else if(year - postYear === 1){
-          when = year - postYear ;
-          postedAgoText = "year ago";
-      }
-      else{
-          when = year - postYear ;
-          postedAgoText = "years ago";
-      }
-
-      $(".posts").html(
-        $(".posts").html() +
-          '<div class="container container_' +
-          postsCount +
-          ' card card-post"><div class="row"><div class=" col-2 col-sm-1"><div class="upVote"><i class="fa fa-caret-up fa-2x" ></i></div><div class="votesNumber">200</div><div class="downVote"><i class="fa fa-caret-down fa-2x" ></i></div></div><div class="col"><a href='+link+' target ="_blank" rel="noopener noreferrer"><div class="row"><div class="col"><p class="owner">Posted by ' +
-          user + ' ' +
-          when +" "+postedAgoText+'</p></div></div><div class="row"><div class="col">' + 
-          message +
-          '</div></div><div class="row feedback-section"><a href="post.html?post='+postId+'"><div class="col"><i class="fas fa-comment-alt">' +
-          commentsCounter +
-          ' comments</i></div></a><div class="col"><i class="fa fa-flag offset-5">' +
-          " report</i></div></div></a></div></div></div>"
-      );
-    });
-  });
-
+  return database.ref("posts").once('value');
 }
